7ab356e80b8c8429765ba192e09b734f8134a5e9
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2020-04-14 15:41:42 +1000

    finished scrollbar task but it's very laggy

--- js/chart_renderer.js
+++ js/chart_renderer.js
@@ -60,13 +60,12 @@ export function createCharts(fileName, signal) {
 			},
 			data: chartData,
 		})
-		console.log(chartDemo);
 		$('.channel-btn > div > .canvasjs-chart-canvas + .canvasjs-chart-canvas').css('position', '');
 		chartDemo.render();
 		$('article').append(`
 	  <div class="chartContainer mb-2" id="${chartId}-container">
       </div>
-    `);
+	`);
 		channel.chart =
 		{
 			plot: new CanvasJS.Chart(`${chartId}-container`, {
@@ -75,25 +74,12 @@ export function createCharts(fileName, signal) {
 				animationEnabled: false,
 				zoomEnabled: true,
 				zoomType: "x",
-				rangeChanging: (e) => {
-					const id = `#${chartId}-slider`;
-					const trigger = e.trigger;
-					$(function () {
-						if (trigger == 'reset') {
-							$(id).slider('destroy');
-						}
-						if (trigger == 'zoom') {
-							$(id).slider({
-								min: signal.recordingTime,
-								max: signal.endTime,
-								value: e.axisX[0].viewportMinimum,
-								slide: function (event, ui) {
-									console.log(ui.value)
-								}
-							});
-							$(id).slider('enable');
-						}
-					});
+				rangeChanged: (e) => {
+					const chart = e;
+					scrollHandler(chart, signal.id, signal.channels, chartId, signal.recordingTime, signal.endTime, channel.chart);
+					if (e.trigger == 'zoom' || e.trigger == 'reset') {
+						syncHandler(e, signal.channels);
+					}
 				},
 				title: {
 					text: channel.name,
@@ -111,7 +97,7 @@ export function createCharts(fileName, signal) {
 			id: chartId
 		}
 		chartsCount++;
-		$(`#${chartId}-container`).append(`<div class="slider" id="${chartId}-slider"></div>`);
+		$(`#${chartId}-container`).append(`<div class="${signal.id}-slider" id="${chartId}-slider"></div>`);
 	});
 	$('#signals-info-menu').append(`
     <button class="signal-info-btn btn dropdown-item" style="white-space:normal;" id="${signalId}-info">
@@ -119,6 +105,75 @@ export function createCharts(fileName, signal) {
     </button>
   `);
 	signalsCount++;
+}
+
+$('.ui-slider-handle').mousedown(() => {
+	console.log(1)
+})
+
+function scrollHandler(e, signalId, channels, chartId, recordingTime, endTime, chart) {
+	const idSelector = `#${chartId}-slider`;
+	let axisX = chart.plot.axisX[0]
+	const classSelector = `.${signalId}-slider`;
+	const trigger = e.trigger;
+	const sliderValue = e.axisX[0].viewportMinimum;
+	let validChart;
+	$(function () {
+		if (trigger == 'reset') {
+			if ($(classSelector).slider()) {
+				$(classSelector).slider('destroy');
+			}
+			axisX.set('viewportMinimum', null);
+			axisX.set('viewportMaximum', null);
+		}
+		if (trigger == 'zoom') {
+			$(classSelector, $(this)).slider({
+				min: recordingTime,
+				max: endTime,
+				value: sliderValue,
+				start: function(e, ui) {
+					const scrollId = this.id.split('-')[0];
+					const plot = channels.find((channel) => {
+						return channel.chart.id == scrollId;
+					})
+					validChart = plot.chart;
+				},
+				slide: function (e, ui) {
+					const dif = axisX.get('viewportMaximum') - axisX.get('viewportMinimum');
+					const start = ui.value;
+					const end = start + dif;
+					validChart.plot.axisX[0].set('viewportMinimum', start)
+					validChart.plot.axisX[0].set('viewportMaximum', end);
+				}
+			});
+		}
+	});
+}
+
+function syncHandler(e, channels) {
+	channels.forEach((channel) => {
+		const chart = channel.chart.plot;
+		const options = chart.options;
+		if (!options.axisX) {
+			options.axisX = {};
+		}
+		if (!options.axisY) {
+			options.axisY = {};
+		}
+		if (e.trigger === "reset") {
+			options.axisX.viewportMinimum = options.axisX.viewportMaximum = null;
+			options.axisY.viewportMinimum = options.axisY.viewportMaximum = null;
+			chart.render();
+		} else {
+			if (chart !== e.chart) {
+				options.axisX.viewportMinimum = e.axisX[0].viewportMinimum;
+				options.axisX.viewportMaximum = e.axisX[0].viewportMaximum;
+				options.axisY.viewportMinimum = e.axisY[0].viewportMinimum;
+				options.axisY.viewportMaximum = e.axisY[0].viewportMaximum;
+				chart.render();
+			}
+		}
+	})
 }
 
 function getChartData(channelData, period, startTime) {
