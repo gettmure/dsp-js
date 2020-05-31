f671e964bf0ff1e527bc35729fdd8d81e8618011
gettmure <warcraft6500@gmail.com>
2020-05-31 16:55:31 +1000

    added random signal modelling

--- js/entities/Signal.js
+++ js/entities/Signal.js
@@ -2,145 +2,221 @@ import { Source } from "./Source.js";
 import { Model } from "./Model.js";
 
 export class Signal extends Source {
-	#createButtons;
-	#getModelData;
+  #createButtons;
+  #getModelData;
 
-	constructor(name, channelsCount, measuresCount, frequency, startTime, id) {
-		super(measuresCount, frequency, startTime, id)
-		this.name = name;
-		this.channelsCount = channelsCount;
-		this.channels = [];
-		this.models = [];
+  constructor(name, channelsCount, measuresCount, frequency, startTime, id) {
+    super(measuresCount, frequency, startTime, id);
+    this.name = name;
+    this.channelsCount = channelsCount;
+    this.channels = [];
+    this.models = [];
 
-		this.#createButtons = () => {
-			const SIGNAL_BUTTON_HTML =
-				`<li class="d-flex justify-content-center signal"> 
+    this.#createButtons = () => {
+      const SIGNAL_BUTTON_HTML = `<li class="d-flex justify-content-center signal"> 
 					<div class="btn-group">
 						<button type="button" class="signal-btn container-fluid btn btn-info" id="${this.id}-btn aria-haspopup="true" aria-expanded="false">
 							${this.name}
 						</button>
 					</div>
-				</li>`
-			const CHANNELS_MENU_HTML = `<div class="channels-menu" id="${this.id}"></div>`;
-			const SIGNAL_INFO_BUTTON_HTML = `<button class="signal-info-btn btn dropdown-item" style="white-space:normal;" id="${this.id}-info">${this.name}</button>`;
+				</li>`;
+      const CHANNELS_MENU_HTML = `<div class="channels-menu" id="${this.id}"></div>`;
+      const SIGNAL_INFO_BUTTON_HTML = `<button class="signal-info-btn btn dropdown-item" style="white-space:normal;" id="${this.id}-info">${this.name}</button>`;
 
-			if ($('.signal-navigation-menu-container').css('display') == 'none') {
-				$('.signal-navigation-menu-container').css('display', 'block');
-			}
+      if ($(".signal-navigation-menu-container").css("display") == "none") {
+        $(".signal-navigation-menu-container").css("display", "block");
+      }
 
-			$('.signal-navigation-menu-container').append(CHANNELS_MENU_HTML);
-			$('#signal-navigation-menu').append(SIGNAL_BUTTON_HTML);
-			$('#signals-info-menu').append(SIGNAL_INFO_BUTTON_HTML);
-			$('.signal-choice').append(`<option class="signal-item" value="${this.id}" id="${this.id.match(/\d+/)[0]}">${this.name}</option>`)
-		}
+      $(".signal-navigation-menu-container").append(CHANNELS_MENU_HTML);
+      $("#signal-navigation-menu").append(SIGNAL_BUTTON_HTML);
+      $("#signals-info-menu").append(SIGNAL_INFO_BUTTON_HTML);
+      $(".signal-choice").append(
+        `<option class="signal-item" value="${this.id}" id="${
+          this.id.match(/\d+/)[0]
+        }">${this.name}</option>`
+      );
+    };
 
-		this.#getModelData = (params, type) => {
-			let data = [];
-			let modelFunction;
-			switch (type) {
-				case 'Delayed single impulse': {
-					modelFunction = (params, step) => {
-						const impulseDelay = params[0];
-						return (step == impulseDelay ? 1 : 0);
-					}
-					break;
-				}
-				case 'Delayed single bounce': {
-					modelFunction = (params, step) => {
-						const bounceDelay = params[0];
-						return (step >= bounceDelay ? 1 : 0);
-					}
-					break;
-				}
-				case 'Decreasing discretized exponent': {
-					modelFunction = (params, step) => {
-						const base = params[0];
-						return Math.pow(base, step)
-					}
-					break;
-				}
-				case 'Discretized sinusoid': {
-					modelFunction = (params, step) => {
-						const amplitude = params[0];
-						const frequency = params[1];
-						const phase = params[2];
-						return amplitude * Math.sin(frequency * step + phase);
-					}
-					break;
-				}
-				case 'Meander': {
-					modelFunction = (params, step) => {
-						const period = params[0];
-						return ((step % period) < (period / 2) ? 1 : -1);
-					}
-					break;
-				}
-				case 'Saw': {
-					modelFunction = (params, step) => {
-						const period = params[0];
-						return ((step % period) / period);
-					}
-					break;
-				}
-				case 'Exponential envelope': {
-					modelFunction = (params, step) => {
-						step = step * this.period / 1000;
-						const amplitude = params[0];
-						const width = params[1];
-						const carrierFrequency = params[2];
-						const initialPhase = params[3];
-						return amplitude * Math.exp(-(step / width)) * Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase);
-					}
-					break;
-				}
-				case 'Balance envelope': {
-					modelFunction = (params, step) => {
-						step = step * this.period / 1000;
-						const amplitude = params[0];
-						const envelopeFrequency = params[1];
-						const carrierFrequency = params[2];
-						const initialPhase = params[3];
-						return amplitude * Math.cos(2 * Math.PI * envelopeFrequency * step) * Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase);
-					}
-					break
-				}
-				case 'Tonal envelope': {
-					modelFunction = (params, step) => {
-						step = step * this.period / 1000;
-						const amplitude = params[0];
-						const envelopeFrequency = params[1];
-						const carrierFrequency = params[2];
-						const initialPhase = params[3];
-						const index = params[4];
-						return amplitude * (1 + index * Math.cos(2 * Math.PI * envelopeFrequency * step)) * Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase);
-					}
-					break
-				}
-			}
-			for (let i = 0; i < this.measuresCount; i++) {
-				const value = modelFunction(params, (i + 1));
-				data.push(value);
-			}
-			return data;
-		}
-	}
+    this.#getModelData = (params, type) => {
+      let data = [];
+      let modelFunction;
+      let normalLawValues = [];
+      switch (type) {
+        case "Delayed single impulse": {
+          modelFunction = (params, step) => {
+            const impulseDelay = params[0];
+            return step == impulseDelay ? 1 : 0;
+          };
+          break;
+        }
+        case "Delayed single bounce": {
+          modelFunction = (params, step) => {
+            const bounceDelay = params[0];
+            return step >= bounceDelay ? 1 : 0;
+          };
+          break;
+        }
+        case "Decreasing discretized exponent": {
+          modelFunction = (params, step) => {
+            const base = params[0];
+            return Math.pow(base, step);
+          };
+          break;
+        }
+        case "Discretized sinusoid": {
+          modelFunction = (params, step) => {
+            const amplitude = params[0];
+            const frequency = params[1];
+            const phase = params[2];
+            return amplitude * Math.sin(frequency * step + phase);
+          };
+          break;
+        }
+        case "Meander": {
+          modelFunction = (params, step) => {
+            const period = params[0];
+            return step % period < period / 2 ? 1 : -1;
+          };
+          break;
+        }
+        case "Saw": {
+          modelFunction = (params, step) => {
+            const period = params[0];
+            return (step % period) / period;
+          };
+          break;
+        }
+        case "Exponential envelope": {
+          modelFunction = (params, step) => {
+            step = (step * this.period) / 1000;
+            const amplitude = params[0];
+            const width = params[1];
+            const carrierFrequency = params[2];
+            const initialPhase = params[3];
+            return (
+              amplitude *
+              Math.exp(-(step / width)) *
+              Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase)
+            );
+          };
+          break;
+        }
+        case "Balance envelope": {
+          modelFunction = (params, step) => {
+            step = (step * this.period) / 1000;
+            const amplitude = params[0];
+            const envelopeFrequency = params[1];
+            const carrierFrequency = params[2];
+            const initialPhase = params[3];
+            return (
+              amplitude *
+              Math.cos(2 * Math.PI * envelopeFrequency * step) *
+              Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase)
+            );
+          };
+          break;
+        }
+        case "Tonal envelope": {
+          modelFunction = (params, step) => {
+            step = (step * this.period) / 1000;
+            const amplitude = params[0];
+            const envelopeFrequency = params[1];
+            const carrierFrequency = params[2];
+            const initialPhase = params[3];
+            const index = params[4];
+            return (
+              amplitude *
+              (1 + index * Math.cos(2 * Math.PI * envelopeFrequency * step)) *
+              Math.cos(2 * Math.PI * carrierFrequency * step + initialPhase)
+            );
+          };
+          break;
+        }
+        case "White noise (interval)": {
+          modelFunction = (params, step) => {
+            const left = params[0][0];
+            const right = params[0][1];
+            return left + (right - left) * Math.random();
+          };
+          break;
+        }
+        case "White noise (normal law)": {
+          modelFunction = (params, step) => {
+            const alpha = params[0];
+            const sigma = Math.sqrt(params[1]);
+            let randomValue = 0;
+            for (let i = 0; i < 12; i++) {
+              randomValue += Math.random();
+            }
+            randomValue -= 6;
+            return alpha + sigma * randomValue;
+          };
+          break;
+        }
+        case "АРСС": {
+          modelFunction = (params, step) => {
+            console.log(params);
+            const sigma = Math.sqrt(params[0]);
+            const aCoefs = params[1];
+            const bCoefs = params[2];
+            const P = aCoefs.length;
+            const Q = bCoefs.length;
+            let randomValue = 0;
+            for (let i = 0; i < 12; i++) {
+              randomValue += Math.random();
+            }
+            randomValue -= 6;
+            normalLawValues.push(sigma * randomValue);
+            if (step == 0) {
+              return normalLawValues[0];
+            } else {
+              let value = normalLawValues[step];
+              for (let i = 1; i <= Q; i++) {
+                value += bCoefs[i - 1] * normalLawValues[step - 1];
+              }
+              for (let i = 1; i <= P; i++) {
+                value += aCoefs[i - 1] * data[step - 1];
+              }
+              return value;
+            }
+          };
+          break;
+        }
+      }
+      for (let i = 0; i < this.measuresCount; i++) {
+        const value = modelFunction(params, i);
+        data.push(value);
+      }
+      return data;
+    };
+  }
 
-	renderChannels() {
-		this.#createButtons();
-		this.channels.forEach((channel) => {
-			channel.renderChart(this.channels)
-		});
-	}
+  renderChannels() {
+    this.#createButtons();
+    this.channels.forEach((channel) => {
+      channel.renderChart(this.channels);
+    });
+  }
 
-	renderModel(type, parameters) {
-		if (this.channels.length == 0 && this.models.length == 0) {
-			this.#createButtons();
-		}
-		const modelIndex = this.channels.length + this.models.length;
-		const modelId = `model-chart${modelIndex}`;
-		const model = new Model(type, this.measuresCount, this.frequency, this.recordingTime, type, modelId, this.id, parameters);
-		model.values = this.#getModelData(parameters, model.type);
-		this.models.push(model);
-		model.renderChart(this.models);
-	}
+  renderModel(type, parameters) {
+    if (this.channels.length == 0 && this.models.length == 0) {
+      this.#createButtons();
+    }
+    const modelIndex = this.channels.length + this.models.length;
+    const modelId = `model-chart${modelIndex}`;
+    const model = new Model(
+      type,
+      this.measuresCount,
+      this.frequency,
+      this.recordingTime,
+      type,
+      modelId,
+      this.id,
+      parameters
+    );
+    model.values = this.#getModelData(parameters, model.type);
+    this.models.push(model);
+    this.channelsCount++;
+    model.renderChart(this.models);
+  }
 }
--- js/parser.js
+++ js/parser.js
@@ -1,41 +1,71 @@
-import { Signal } from './entities/Signal.js';
-import { Channel } from './entities/Channel.js';
+import { Signal } from "./entities/Signal.js";
+import { Channel } from "./entities/Channel.js";
 
 let channelIndex = 0;
 
 export function parseDateTime(date) {
-	const dateElements = date.split(/\D/);
-	return new Date(dateElements[0], dateElements[1] - 1, dateElements[2], dateElements[4], dateElements[5], dateElements[6], dateElements[7]);
+  const dateElements = date.split(/\D/);
+  dateElements.filter((element) => element != "" || element != null);
+  return new Date(
+    dateElements[0],
+    dateElements[1] - 1,
+    dateElements[2],
+    dateElements[3],
+    dateElements[4],
+    dateElements[5],
+    dateElements[6]
+  );
 }
 
 export function parseTxtFile(signalName, fileContent, signalIndex) {
-	const fileData = fileContent.split('\n').filter(line => line[0] != '#');
-	const channelsCount = parseInt(fileData[0]);
-	const measuresCount = parseInt(fileData[1]);
-	const frequency = parseFloat(fileData[2]);
-	const startDate = fileData[3].replace(/(\d{2})-(\d{2})-(\d{4})/, "$3-$2-$1");
-	const startTime = fileData[4];
-	const date = `${startDate} ${startTime}`;
-	const unixtime = parseDateTime(date).getTime();
-	const signalId = `signal${signalIndex}`;
-	const signal = new Signal(signalName, channelsCount, measuresCount, frequency, unixtime, signalId);
-	fileData[5].split(';').forEach(channelName => {
-		if (channelName == "") {
-			return;
-		}
-		const channelId = `chart${channelIndex}`;
-		signal.channels.push(new Channel(channelName, measuresCount, frequency, unixtime, channelId, signalId));
-		channelIndex++;
-	});
-	for (let lineIndex = 6; lineIndex < fileData.length; lineIndex++) {
-		if (fileData[lineIndex] == '') {
-			break;
-		}
-		for (let channelIndex = 0; channelIndex < signal.channelsCount; channelIndex++) {
-			const data = fileData[lineIndex].split(' ');
-			signal.channels[channelIndex].values.push(parseFloat(data[channelIndex]));
-		}
-	}
-	signalIndex++;
-	return signal;
+  const fileData = fileContent.split("\n").filter((line) => line[0] != "#");
+  const channelsCount = parseInt(fileData[0]);
+  const measuresCount = parseInt(fileData[1]);
+  const frequency = parseFloat(fileData[2]);
+  const startDate = fileData[3].replace(/(\d{2})-(\d{2})-(\d{4})/, "$3-$2-$1");
+  const startTime = fileData[4];
+  const date = `${startDate} ${startTime}`;
+  const unixtime = parseDateTime(date).getTime();
+  const signalId = `signal${signalIndex}`;
+  const signal = new Signal(
+    signalName,
+    channelsCount,
+    measuresCount,
+    frequency,
+    unixtime,
+    signalId
+  );
+  fileData[5].split(";").forEach((channelName) => {
+    if (channelName == "") {
+      return;
+    }
+    const channelId = `chart${channelIndex}`;
+    signal.channels.push(
+      new Channel(
+        channelName,
+        measuresCount,
+        frequency,
+        unixtime,
+        channelId,
+        signalId
+      )
+    );
+    channelIndex++;
+  });
+  for (let lineIndex = 6; lineIndex < fileData.length; lineIndex++) {
+    if (fileData[lineIndex] == "") {
+      break;
+    }
+    for (
+      let channelIndex = 0;
+      channelIndex < signal.channelsCount;
+      channelIndex++
+    ) {
+      const data = fileData[lineIndex].split(" ");
+      data.pop();
+      signal.channels[channelIndex].values.push(parseFloat(data[channelIndex]));
+    }
+  }
+  signalIndex++;
+  return signal;
 }
